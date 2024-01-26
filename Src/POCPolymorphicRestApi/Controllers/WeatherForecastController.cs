3330918e63e8e06afabdb3728d717555873f8456
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-01-26 19:49:08 +0000

    Add projects (#10)

---
+++ Src/POCPolymorphicRestApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace POCPolymorphicRestApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    };
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        logger.LogInformation("GetWeatherForecast called");
+        return Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
---
+++ Tests/POCPolymorphicRestApi.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using POCPolymorphicRestApi.Controllers;
+using Xunit;
+
+namespace POCPolymorphicRestApi.Tests.Controllers;
+
+public class WeatherForecastControllerTests
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastControllerTests()
+    {
+        _logger = Substitute.For<ILogger<WeatherForecastController>>();
+    }
+
+    private WeatherForecastController CreateWeatherForecastController()
+    {
+        return new WeatherForecastController(_logger);
+    }
+
+    [Fact]
+    public void Get_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var weatherForecastController = CreateWeatherForecastController();
+
+        // Act
+        var result = weatherForecastController.Get();
+
+        // Assert
+        result.Should().NotBeNull();
+    }
+}
